e225a5515bfa76b4c2b5433527d9c3e0281f1e1b
SparkyFalls <cjf2988@gmail.com>
2025-01-09 14:37:04 +0800

    Update JavaFoodSystem.java

--- Java Food System/src/java/food/system/JavaFoodSystem.java
+++ Java Food System/src/java/food/system/JavaFoodSystem.java
@@ -16,6 +16,7 @@ public class JavaFoodSystem {
     public static void main(String[] args) {
         System.out.println("hello");
         alks
+               jfidjfdif
         // TODO code application logic here
     }
     
