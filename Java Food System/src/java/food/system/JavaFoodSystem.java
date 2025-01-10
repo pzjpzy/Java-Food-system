1fd5eca53669531adf1c9d4e9f3267376f12afa4
Nour 3 <nourrhafid@gmail.com>
2025-01-10 12:27:18 +0800

    kk

--- Java Food System/src/java/food/system/JavaFoodSystem.java
+++ Java Food System/src/java/food/system/JavaFoodSystem.java
@@ -15,8 +15,6 @@ public class JavaFoodSystem {
      */
     public static void main(String[] args) {
         System.out.println("hello");
-        alks
-               jfidjfdif
         // TODO code application logic here
     }
     
