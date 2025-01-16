b0ae77fdbdd56b53a0a63093d6483d5c722ef34d
pzjpzy <pangzaijie@gmail.com>
2025-01-16 18:46:51 +0800

    divide main

---
+++ Java Food System/src/Customer/frame2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Customer;
+
+import javax.swing.JFrame;
+import static javax.swing.WindowConstants.EXIT_ON_CLOSE;
+
+/**
+ *
+ * @author pangz
+ */
+public class frame2 extends JFrame{
+    public frame2() {
+        selectVendor panelz  = new selectVendor(this);           //put your login panel here, you're putting panel into this frame
+
+        
+        setTitle("Ordering Food System");
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(false);
+        setLayout(null);
+        setExtendedState(JFrame.MAXIMIZED_BOTH);
+        add(panelz);
+      
+        }
+}
---
+++ Java Food System/src/Customer/zai_jie_Main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Customer;
+
+/**
+ *
+ * @author pangz
+ */
+public class zai_jie_Main {
+    public static void main(String[] args) {
+         frame2 frame2 = new frame2();   
+    }
+}
